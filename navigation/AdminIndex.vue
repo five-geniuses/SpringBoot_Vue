0a0a3bb6be27e1a41dcd8405eeaa31cd396504e5
LYan-07 <2786551040@qq.com>
2025-06-10 10:45:52 +0800

    Add files via upload

---
+++ navigation/AdminIndex.vue
@@ -0,0 +1,17 @@
+<!-- views/admin/AdminIndex.vue -->
+<template>
+  <div class="admin-index">
+    <h2>管理员首页</h2>
+    <p>欢迎使用网上商城后台管理系统</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.admin-index {
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ navigation/UserIndex.vue
@@ -0,0 +1,17 @@
+<!-- src/views/user/UserIndex.vue -->
+<template>
+  <div class="user-index">
+    <h2>用户首页</h2>
+    <!-- 具体内容 -->
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.user-index {
+  text-align: center;
+  padding: 20px;
+}
+</style>
