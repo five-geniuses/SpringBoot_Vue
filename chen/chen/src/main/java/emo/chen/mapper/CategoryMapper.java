104627538d331cd695f8d889799de24de706731f
cy-zhiwu <3458981693@qq.com>
2025-06-11 15:12:01 +0800

    Delete chen directory

--- chen/chen/src/main/java/emo/chen/mapper/CategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package emo.chen.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import emo.chen.entity.Category;
-import org.apache.ibatis.annotations.Mapper;
- 
-@Mapper
-public interface CategoryMapper extends BaseMapper<Category> {
-} 
